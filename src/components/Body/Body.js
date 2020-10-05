86b89fa3fdfd783ba04087d17f3db5e09c75c34f
NekoCarreraDesigns <denvermaas@gmail.com>
2020-10-05 16:50:06 -0600

    making revisions to portfolio page

--- src/components/Body/Body.js
+++ src/components/Body/Body.js
@@ -4,14 +4,15 @@ import "./Body.css";
 function Body() {
     return (
         <div className="sea-and-sand" >
-            <p className="intro">Hello My name is Nicholas C. Maas, I am a graduate of the University of Denver Coding Boot Camp. I am a Full Stack Web Developer, I love to design, and enjoy the front-end because of my art background.
-            I do enjoy the functionality of the Back end as well. Apps are made with the MERN stack, Currently learning Python, and Angular. Skills include rigorous testing, debugging, MVC, JSON, user authentication, coding, and reading of documentation, ORM, creating REST API.</p>
-            <img className="sunset-surf" alt="sunset-surfer" src="https://images.unsplash.com/photo-1475615787683-f1a161b002b1?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=60"></img>
-            <img className="aerial" alt="aerial-san-diego" src="https://images.unsplash.com/photo-1544632795-2e45e65a8ce4?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=60"></img>
             <img className="nicholas" alt="handsome-gentleman" src="./assets/Nicholas1.jpeg"></img>
-            <p className="intro" >Technologies I know: HTML5, CSS3, Bootstrap, Semantic UI,
-            Reactstrap, Materialize, Handlebars, Mongo, Express, Node, React, jQuery, AJAX, JavaScript, ES6, MySQL, Heroku, Github, GitBash, Jest. Have some legacy code that needs an update? I am also
-            well versed in PWA, Not getting the search results you like? I am a sucker for some good SEO as well</p>
+            <img className="tech-1" alt="technology" src="https://images.unsplash.com/photo-1461749280684-dccba630e2f6?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=60"></img>
+            <img className="tech-1" alt="technology" src="https://images.unsplash.com/photo-1457305237443-44c3d5a30b89?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=60"></img>
+            <p className="intro-1">Hello My name is Nicholas C. Maas, I am a graduate of the University of Denver Coding Boot Camp. I am a Full Stack Web Developer, I love to design, and enjoy the front-end because of my art background.
+            I do enjoy the functionality of the Back end as well. Apps are made with the MERN stack, Currently learning Python, and Angular. Skills include rigorous testing, debugging, MVC, JSON, user authentication, coding, and reading of documentation, ORM, creating REST API.</p>
+
+            <p className="intro-1" >Technologies I know: HTML5, CSS3, Bootstrap, Semantic UI,
+            Reactstrap, Materialize, Handlebars, MongoDB, Express, Node, React, jQuery, AJAX, JavaScript, ES6, MySQL, Heroku, Github, GitBash, Jest. Have some legacy code that needs an update? I am also
+            well versed in Progressive Web Apps, Not getting the search results you like? I am a sucker for some good Search Engine Optimization as well</p>
         </div>
 
     )
