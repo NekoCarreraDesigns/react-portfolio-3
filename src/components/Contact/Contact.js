1dc86dbf3b0dbac4421aae79342d385d82fa05d5
NekoCarreraDesigns <denvermaas@gmail.com>
2020-10-13 19:44:37 -0600

    added new version to package.json, fixed contact page

--- src/components/Contact/Contact.js
+++ src/components/Contact/Contact.js
@@ -6,12 +6,21 @@ function Contact() {
         <div className="contact-me">
             <ul className="list-unstyled">
                 <h2 className="holla-at-me">Contact Me</h2>
-                <p className="holla-at-me">Text or Call me at 303-931-0974, not feeling that option, e-mail me at denvermaas@gmail.com</p>
-                <li><i className="fab fa-instagram-square" /><a rel="noopener noreferrer" className="button2" target="_blank" href="https://www.instagram.com/LaVeyathan78">Instagram</a></li>
-                <li><i className="fab fa-linkedin" /><a rel="noopener noreferrer" className="button2" target="_blank" href="https://www.linkedin.com/in/nekocarrera1978">Linkedin</a></li>
-                <li><i className="fas fa-file" /><a rel="noopener noreferrer" className="button2" target="_blank" href="https://1drv.ms/w/s!BBBgULp6C0cCgxhpO_w6NsZDzlAH?e=ppaGqH">Resume´</a>
-                    <li><i className="fab fa-github-square" /><a rel="noopener noreferrer" className="button2" target="_blank" href="https://github.com/NekoCarreraDesigns">Github</a></li>
-                </li>
+                <br></br>
+                <p className="holla-at-me">Text or Call me at 303-931-0974</p>
+                <p className="holla-at-me">e-mail me at denvermaas@gmail.com</p>
+                <br></br>
+                <div>
+                    <i className="icon fab fa-instagram-square" /><a rel="noopener noreferrer" className="button2" target="_blank" href="https://www.instagram.com/LaVeyathan78">Instagram</a>
+                    <br></br>
+                    <i className="icon fab fa-linkedin" /><a rel="noopener noreferrer" className="button2" target="_blank" href="https://www.linkedin.com/in/nekocarrera1978">Linkedin</a>
+                    <br></br>
+                    <i className="icon fas fa-file" /><a rel="noopener noreferrer" className="button2" target="_blank" href="https://docs.google.com/document/d/1r9NBjCkj_Q5LOli6ad242eGSwj20QfrdPi89Oz9QqGg/edit?usp=sharing">Resume´</a>
+                    <br></br>
+                    <i className="icon fab fa-github-square" /><a rel="noopener noreferrer" className="button2" target="_blank" href="https://github.com/NekoCarreraDesigns">Github</a>
+                    <br></br>
+
+                </div>
             </ul>
         </div>
     )
