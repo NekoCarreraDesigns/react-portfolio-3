856273f641407ef0d40813d3f5ac00297156b500
NekoCarreraDesigns <denvermaas@gmail.com>
2020-10-13 18:32:07 -0600

    fixed whitespace issue on bottom of landing page

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -4,7 +4,7 @@ import "./Header.css";
 function Header(props) {
     return (
         <div>
-            <h1 className="page-head container-fluid">
+            <h1 className="page-head">
                 <img className="page-head img-fluid" alt="computer-code" src="https://images.unsplash.com/photo-1571171637578-41bc2dd41cd2?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=60"></img>
                 Welcome to Neko Carrera Designs
             </h1>
