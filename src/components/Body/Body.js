179973aeadb1147dcd6e2d3dc5c2f12c97960a63
NekoCarreraDesigns <denvermaas@gmail.com>
2020-10-14 22:03:30 -0600

    fixed images to fit on page, swapped my picture from top to middle,and adjusted the padding on the contact page, tried a media query for body

--- src/components/Body/Body.js
+++ src/components/Body/Body.js
@@ -3,9 +3,9 @@ import "./Body.css";
 
 function Body() {
     return (
-        <div className="sea-and-sand" >
-            <img className="nicholas" alt="handsome-gentleman" src="./assets/Nicholas1.jpeg"></img>
+        <div size="fluid" className="sea-and-sand" >
             <img className="tech-1" alt="technology" src="https://images.unsplash.com/photo-1461749280684-dccba630e2f6?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=60"></img>
+            <img className="nicholas" alt="handsome-gentleman" src="./assets/Nicholas1.jpeg"></img>
             <img className="tech-1" alt="technology" src="https://images.unsplash.com/photo-1457305237443-44c3d5a30b89?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=60"></img>
             <p className="intro-1">Hello My name is Nicholas C. Maas, I am a graduate of the University of Denver Coding Boot Camp. I am a Full Stack Web Developer, I love to design, and enjoy the front-end because of my art background.
             I do enjoy the functionality of the Back end as well. Apps are made with the MERN stack, Currently learning Python, and Angular. Skills include rigorous testing, debugging, MVC, JSON, user authentication, coding, and reading of documentation, ORM, creating REST API.</p>
